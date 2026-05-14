68f3e9fb0b99e4fc33ce8f25d406257c24ba46d2
Joe Alcock <j.alcock@2023.ljmu.ac.uk>
2026-05-14 12:43:45 +0100

    started preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,20 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def load_and_preprocess_data():
+    data = pd.read_csv('Energy_Consumption.csv')
+    data = data.dropna()
+    X = data.drop('Energy_Consumption', axis=1)
+    y = data['Energy_Consumption']
+    X_train, X_test, y_train, y_test = train_test_split(X,y)
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test, y_train, y_test, scaler
+
+def preprcess_new_data(new_data, scaler):
+    new_data_scaled = scaler.transform(new_data)
+    return new_data_scaled
 
